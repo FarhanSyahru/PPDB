bba5acca25c1816d081eef3f06234ab6b6fb4a31
FarhanSyahru <ramadanfarhan215@gmail.com>
2023-03-24 10:50:20 +0700

    first commit

---
+++ application/controllers/Admin.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller 
+{
+    public function index()
+    {
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function input()
+    {
+        $data['title'] = 'User Input';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/input', $data);
+        $this->load->view('templates/footer'); 
+    }
+
+    public function prosesInput()
+    {
+        
+    }
+
+}
